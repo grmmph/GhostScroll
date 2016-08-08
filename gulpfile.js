6f58be96831ee40d8dfc154e550721ef0e9c4546
hbaughman <hbaughman741@gmail.com>
2016-08-08 15:52:20 -0400

    Critical CSS inlined

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
+var fs = require('fs')
 var gulp = require('gulp')
 
-var cssnano = require('gulp-cssnano')
+var cssnano = require('cssnano')
+var gulpCssNano = require('gulp-cssnano')
 var sass = require('gulp-sass')
+var penthouse = require('penthouse')
 
 gulp.task('sass', function() {
   return gulp.src('assets/sass/*.+(sass|scss)')
     .pipe(sass().on('error', sass.logError))
-    .pipe(cssnano())
+    .pipe(gulpCssNano())
     .pipe(gulp.dest('assets/css/'))
+})
+
+gulp.task('penthouse', function() {
+  penthouse({
+    url: "http://localhost:2368",
+    css: "assets/css/main.css",
+    width: 320,
+    height: 568
+  }, function(err, critical) {
+    if(err) console.error(err)
+    cssnano.process(critical, {}).then(function(result) {
+      var wrappedCritical = "<style>" + result + "</style>"
+      fs.writeFile('partials/critical-css.hbs', wrappedCritical)
+    })
+  })
 })
 
 gulp.task('build', [ 'sass' ])
@@ -15,6 +33,7 @@ gulp.task('build', [ 'sass' ])
 gulp.task('watch', function() {
   gulp.watch('assets/sass/**/*.+(sass|scss)', ['sass'])
 })
+
 
 gulp.task('default', [ 'build', 'watch' ])
 
